ddeb37cd2c7cef33a48d99f8193734a57fcb40bd
Euheny Dashkevich <euheny.dashkevich@itechart-group.com>
2017-05-18 15:07:46 +0300

    installed webium

--- stratifi_tests/test_login.py
+++ stratifi_tests/test_login.py
@@ -10,15 +10,16 @@ from pages.advisors_stratifi_pages.stratifi_page import StratifiPage
 
 class TestLogin(BaseTest):
     # stratifi_page = StratifiPage()
-    sign_in_page = SignInPage()
+
 
     def test_login(self):
-        self.sign_in_page.open()
-        self.sign_in_page.enter_login('akhil@stratifi.com')
-        self.sign_in_page.enter_password('Hell0w0rld123!')
-        self.sign_in_page.press_sign_in()
-        # stratifi_page = StratifiPage()
-        # stratifi_page.check_if_page_is_loaded()
+        sign_in_page = SignInPage()
+        sign_in_page.open()
+        sign_in_page.enter_login('akhil@stratifi.com')
+        sign_in_page.enter_password('Hell0w0rld123!')
+        sign_in_page.press_sign_in()
+        stratifi_page = StratifiPage()
+        stratifi_page.check_if_page_is_loaded()
         # print('Successfully logged in: ' + str(stratifi_page.is_element_present('startAnalise', timeout=60)))
 
 
