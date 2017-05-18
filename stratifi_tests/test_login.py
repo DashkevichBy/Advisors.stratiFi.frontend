56e438171f1add6690f95db9a9f6e420a09e85e6
Euheny Dashkevich <euheny.dashkevich@itechart-group.com>
2017-05-18 12:35:01 +0300

    returned assert

--- stratifi_tests/test_login.py
+++ stratifi_tests/test_login.py
@@ -7,9 +7,6 @@ from common import BaseTest
 from pages.advisors_stratifi_pages.sign_in_page import SignInPage
 from pages.advisors_stratifi_pages.stratifi_page import StratifiPage
 
-logging.basicConfig(level=logging.INFO)
-mylogger = logging.getLogger()
-
 
 class TestLogin(BaseTest):
     stratifi_page = StratifiPage()
@@ -20,7 +17,7 @@ class TestLogin(BaseTest):
         self.sign_in_page.enter_login('akhil@stratifi.com')
         self.sign_in_page.enter_password('Hell0w0rld123!')
         self.sign_in_page.press_sign_in()
-        # self.stratifi_page.check_if_page_is_loaded()
+        self.stratifi_page.check_if_page_is_loaded()
         # print('Successfully logged in: ' + str(stratifi_page.is_element_present('startAnalise', timeout=60)))
 
 
